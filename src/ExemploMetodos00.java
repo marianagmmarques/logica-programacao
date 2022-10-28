8e9a9b551a490167a3576a8745f8f1e9dd0330e7
marianagmmarques <marigmmarques@gmail.com>
2022-10-27 21:10:55 -0300

    Alterando e melhorando array 4

---
+++ src/ExemploMetodos00.java
@@ -0,0 +1,17 @@
+public class ExemploMetodos00 {
+    public static void main(String[] args) {
+        //System.out.println("Teste".equals("TESTE"));
+        //System.out.println("Teste".equals("Teste"));
+        boasVindas();
+        boasVindas();
+        boasVindas();
+        boasVindas();
+        boasVindas();
+        boasVindas();
+    }
+
+    static void boasVindas(){
+
+        System.out.println("Seja bem vindo!");
+    }
+}
