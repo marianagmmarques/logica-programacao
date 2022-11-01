16b6fc5b5122dfda91060e13b958d851bebb7df5
marianagmmarques <marigmmarques@gmail.com>
2022-10-31 21:30:20 -0300

    Adicionando Pastas Específicas e POO

---
+++ src/ExemploMetodos/ExemploMetodos02.java
@@ -0,0 +1,14 @@
+public class ExemploMetodos02 {
+    public static void main(String[] args) {
+        int nFinal = somaRecursiva(15);
+        System.out.println(nFinal);
+    }
+
+    static int somaRecursiva(int n){
+        if (n > 0) {
+            return n + somaRecursiva(n - 1);
+        } else {
+            return 0;
+        }
+    }
+}
